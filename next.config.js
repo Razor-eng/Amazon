9e192c529fd5f13928149ee994ce3a6cbead092e
Razor-eng <83156526+Razor-eng@users.noreply.github.com>
2023-03-21 03:21:05 +0530

    app build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images:{
-    domains:["links.papareact.com","fakestoreapi.com","www.linkpicture.com"],
+    domains:["links.papareact.com","fakestoreapi.com"],
   }
 }
 
